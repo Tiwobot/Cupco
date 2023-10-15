2c49245966a2f66bd72c8735dc8be2063372f28e
tiwobot <twogun.pre@gmail.com>
2023-10-15 22:22:32 +0300

    Add to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cupco test WebServer</h1>
+        <p>Get ready to experience a unique coffee!</p>
       </header>
+      <main className="App-content">
+        <section className="App-section">
+          <h2>About</h2>
+          <p>This is a papercup, a papercup to captivate your imagination.</p>
+        </section>
+        <section className="App-section">
+          <h2>Contact</h2>
+          <p>Feel free to reach out to us for any inquiries or feedback.</p>
+        </section>
+      </main>
+      <footer className="App-footer">
+        <p>© 2023 Cupco. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
